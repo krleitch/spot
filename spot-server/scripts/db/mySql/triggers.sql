a223e0622dacab9a5114cbde5bf81ea6bf3863f5
Kevin <krleitch.ca@gmail.com>
2020-02-12 01:54:19 -0500

    add more triggers for score

--- spot-server/scripts/db/mySql/triggers.sql
+++ spot-server/scripts/db/mySql/triggers.sql
@@ -1,4 +1,7 @@
 DROP TRIGGER IF EXISTS posts_insert;
+DROP TRIGGER IF EXISTS comments_insert;
+DROP TRIGGER IF EXISTS likes_post_insert;
+DROP TRIGGER IF EXISTS likes_comments_insert;
 
 delimiter $$
 create trigger posts_insert
@@ -6,5 +9,26 @@ after insert on posts
 for each row
 begin
     update accounts set score = score + 1 where id = new.account_id;
+end$$
+
+create trigger comments_insert
+after insert on comments
+for each row
+begin
+    update accounts set score = score + 1 where id = new.account_id;
+end$$
+
+create trigger likes_post_insert
+after insert on posts_rating
+for each row
+begin
+    update accounts set score = score + 1 where id = (SELECT account_id FROM posts WHERE id = new.post_id);
+end$$
+
+create trigger likes_comments_insert
+after insert on comments_rating
+for each row
+begin
+    update accounts set score = score + 1 where id = (SELECT account_id FROM comments WHERE id = new.comment_id);
 end$$
 delimiter ;
