88bf2262fbde921d0fc46c31be0735bfee29642e
Kevin <krleitch.ca@gmail.com>
2020-02-18 00:10:25 -0500

    change time to function

--- spot-web/src/app/components/main/post/post.component.ts
+++ spot-web/src/app/components/main/post/post.component.ts
@@ -31,7 +31,6 @@ export class PostComponent implements OnInit {
   MAX_POST_LENGTH = 300;
   expanded = false;
 
-  timeMessage: string;
   optionsEnabled = false;
 
   constructor(private store$: Store<RootStoreState.State>, private router: Router, private postsService: PostsService) {
@@ -39,8 +38,6 @@ export class PostComponent implements OnInit {
   }
 
   ngOnInit() {
-
-    this.getTime(this.post.creation_date);
 
     this.location$ = this.store$.pipe(
       select(AccountsStoreSelectors.selectAccountsLocation)
@@ -75,35 +72,35 @@ export class PostComponent implements OnInit {
     this.router.navigateByUrl('posts/' + this.post.id);
   }
 
-  getTime(date) {
+  getTime() {
     const curTime = new Date();
-    const postTime = new Date(date);
+    const postTime = new Date(this.post.creation_date);
     const timeDiff = curTime.getTime() - postTime.getTime();
     if (timeDiff < 60000) {
       const secDiff = Math.round(timeDiff / 1000);
       if (secDiff === 0) {
-        this.timeMessage = 'Now';
+        return 'Now';
       } else {
-        this.timeMessage = secDiff + 's';
+        return secDiff + 's';
       }
     } else if (timeDiff < 3600000) {
       const minDiff = Math.round(timeDiff / 60000);
-      this.timeMessage = minDiff + 'm';
+      return minDiff + 'm';
     } else if (timeDiff < 86400000) {
       const hourDiff = Math.round(timeDiff / 3600000);
-      this.timeMessage = hourDiff + 'h';
+      return hourDiff + 'h';
     } else if (timeDiff < 31536000000) {
       const dayDiff = Math.round(timeDiff / 86400000);
-      this.timeMessage = dayDiff + 'd';
+      return dayDiff + 'd';
     } else {
       const yearDiff = Math.round(timeDiff / 31536000000);
-      this.timeMessage = yearDiff + 'y';
+      return yearDiff + 'y';
     }
   }
 
   getDistance() {
     return this.postsService.calcDistance(this.post.latitude, this.post.longitude, this.myLocation.latitude,
-                                           this.myLocation.longitude, 'M').toFixed(1) + 'm';
+                                           this.myLocation.longitude, 'M').toFixed(1) + ' miles';
   }
 
   getContent(): string {
