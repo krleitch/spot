4f0590c649c1e416d8143f1d84da0548e7c164e4
krleitch <krleitch.ca@gmail.com>
2020-03-18 21:41:47 -0400

    posts distance needs go change

--- spot-server/src/app/routes/posts.ts
+++ spot-server/src/app/routes/posts.ts
@@ -21,6 +21,8 @@ router.get('/', function (req: any, res: any) {
     const offset = Number(req.query.offset);
     const limit = Number(req.query.limit);
 
+    // https://gis.stackexchange.com/questions/31628/find-points-within-a-distance-using-mysql
+    // USE THIS
     posts.getPosts(accountId, sort, offset, limit).then((rows: any) => {
         rows = rows.filter( ( post: any ) => postsService.filterLocation(post, location, latitude, longitude));
         res.status(200).json({ posts: rows });
