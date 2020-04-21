24281c216d8ee9042429e434b8430732946e43ad
Kevin <krleitch.ca@gmail.com>
2020-04-21 14:34:53 -0400

    basic is there/ style later

--- spot-server/src/app/routes/image.ts
+++ spot-server/src/app/routes/image.ts
@@ -12,8 +12,6 @@ router.use(function timeLog (req: any, res: any, next: any) {
 // Upload a photo
 router.post('/upload', function (req: any, res: any) {
 
-    console.log('got it');
-
     singleUpload(req, res, function(err: any) {
 
         if (err) {
