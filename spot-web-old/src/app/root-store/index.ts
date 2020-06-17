853e1acc71611c428353642ef2882f8385d1e8b4
Kevin <krleitch.ca@gmail.com>
2020-06-17 14:42:37 -0400

    remove old spot web

--- spot-web-old/src/app/root-store/index.ts
+++
@@ -1,9 +0,0 @@
-import { RootStoreModule } from './store.module';
-import * as RootStoreSelectors from './selectors';
-import * as RootStoreState from './state';
-
-export * from './posts-store';
-export * from './accounts-store';
-export * from './comments-store';
-
-export { RootStoreState, RootStoreSelectors, RootStoreModule };
--- spot-web-old/src/app/root-store/selectors.ts
+++
@@ -1,3 +0,0 @@
-export { };
-
-// Selectors for global state go here
