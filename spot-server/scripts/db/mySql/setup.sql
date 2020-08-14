f5cb33cacc6e38c5ff27dfa4133a967b5f960757
Kevin <krleitch.ca@gmail.com>
2020-08-13 20:52:19 -0400

    make sql setup file

---
+++ spot-server/scripts/db/mySql/setup.sql
@@ -0,0 +1,6 @@
+
+source C:/Users/Kevin/Documents/repos/spot/spot-server/scripts/db/mySql/create.sql;
+
+source C:/Users/Kevin/Documents/repos/spot/spot-server/scripts/db/mySql/accountsTriggers.sql;
+source C:/Users/Kevin/Documents/repos/spot/spot-server/scripts/db/mySql/commentsTriggers.sql;
+source C:/Users/Kevin/Documents/repos/spot/spot-server/scripts/db/mySql/postsTriggers.sql;
