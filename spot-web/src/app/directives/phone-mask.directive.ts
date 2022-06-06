b7c00cedfb70216e846a7debf314b80b009d22ba
krleitch <krleitch.ca@gmail.com>
2022-06-06 18:24:27 -0400

    add phone validator to all phone fields

--- spot-web/src/app/directives/phone-mask.directive.ts
+++ spot-web/src/app/directives/phone-mask.directive.ts
@@ -36,7 +36,7 @@ export class PhoneMaskDirective {
     } else if (newVal.length <= 10) {
       newVal = newVal.replace(/^(\d{0,3})(\d{0,3})(\d{0,4})/, '($1) $2-$3');
     } else {
-      newVal = newVal.substring(0, 10);
+      newVal = newVal.substring(0, 9);
       newVal = newVal.replace(/^(\d{0,3})(\d{0,3})(\d{0,4})/, '($1) $2-$3');
     }
     this.ngControl.valueAccessor.writeValue(newVal);
--- spot-web/src/app/helpers/validators/validate-helpers.ts
+++ spot-web/src/app/helpers/validators/validate-helpers.ts
@@ -20,8 +20,7 @@ export function validateAllFormFields(formGroup: FormGroup) {
 // just check for an @ and a .
 export const VALID_EMAIL_REGEX = /^\S+@\S+\.\S+$/;
 // correct number of digits in either form
-export const VALID_PHONE_REGEX =
-  /^\s*(?:\+?(\d{1,3}))?[-. (]*(\d{3})[-. )]*(\d{3})[-. ]*(\d{4})(?: *x(\d+))?\s*$/;
+export const VALID_PHONE_REGEX = /^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}$/;
 // alphanumeric starting with a letter (_ and - and . allowed)
 export const VALID_USERNAME_REGEX = /^[a-zA-Z][a-zA-Z0-9_\-\.]*$/;
 // one letter, one number, and one special character
