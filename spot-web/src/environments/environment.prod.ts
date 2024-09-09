21d0bde4a131b266a9ceda4e844f621e91b3a468
Kevin Leitch <krleitch.ca@gmail.com>
2024-09-09 16:16:34 -0400

    Update environment.prod.ts

--- spot-web/src/environments/environment.prod.ts
+++ spot-web/src/environments/environment.prod.ts
@@ -4,5 +4,5 @@ export const environment = {
   chatBaseUrl: '/chat/api',
   websocketUrl: 'wss://spottables.com',
   googleProviderId:
-    '562475424621-665s55gudfsaq3usnb140dhmlfhs1ho1.apps.googleusercontent.com'
+    ''
 };
